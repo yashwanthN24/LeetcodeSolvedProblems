a2d7fa85cb1aa3cc805dad29b00489bfc9ca3dfc
yashwanthN24 <106897859+yashwanthN24@users.noreply.github.com>
2024-04-29 18:46:38 +0530

    Time: 11 ms (42.24%) | Memory: 57.6 MB (7.28%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+
+    public ListNode getMid(ListNode head){
+        ListNode slow = head ; 
+        ListNode fast = head.next ; 
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next ; 
+        }
+        return slow;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null){
+            return head ; 
+        }
+
+        // getMid Node 
+        ListNode midNode = getMid(head);
+
+        ListNode rightHead = midNode.next ; 
+
+        midNode.next = null ;
+
+        ListNode newleft = sortList(head);
+        ListNode newright = sortList(rightHead);
+
+        return merge(newleft , newright);
+    }
+
+    public ListNode merge(ListNode left , ListNode right ){
+        ListNode mergedLL = new ListNode(-1);
+        ListNode temp = mergedLL ; 
+
+        while(left != null && right != null){
+            if(left.val < right.val ){
+                temp.next = left ; 
+                left = left.next ; 
+                temp = temp.next ; 
+            }else{
+                temp.next = right ;
+                right = right.next ; 
+                temp = temp.next ; 
+            }
+        }
+
+        while(left != null ){
+            temp.next = left ; 
+            left = left.next ; 
+            temp = temp.next ;
+        }
+
+        while(right != null){
+            temp.next = right ;
+            right = right.next ; 
+            temp = temp.next ; 
+        }
+
+        return mergedLL.next ; 
+    }
+}
