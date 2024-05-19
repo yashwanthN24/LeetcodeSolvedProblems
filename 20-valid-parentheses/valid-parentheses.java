aa97ba01d5bbaae0ecd56d9ce2c7d80878ba5274
yashwanthN24 <106897859+yashwanthN24@users.noreply.github.com>
2024-05-19 11:56:05 +0530

    Time: 1 ms (98.72%) | Memory: 41.1 MB (85.84%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,21 +5,25 @@ class Solution {
         for(int i = 0 ;  i<s.length(); i++){
             char ch = s.charAt(i);
 
-            // if ch is open bracket push on to stack 
+            // if ch is close bracket 
             if(ch == ')' || ch == ']' || ch == '}'){
+
+                // to handle case when the string is only )))))) closed brackets then stack would be empty 
                 if(st.empty()){
                     return false ; 
                 }
                 char topchar = st.pop();
+                // checking for pair () , {} , []
                 if((topchar != '(' && ch == ')') || (topchar != '[' && ch == ']') || (topchar != '{' && ch ==                 '}' )){
                         return false ;
                 }
             }
             else{
+                // if its open bracket ( , { , [
                 st.push(ch);
             }
         }
-
+        //  we say its valid prathesis when the stack becomes empty i.e as find balanced parenthesis for all 
         return st.isEmpty();
     }
 }
