043cbc83c51d8495a4b90463b9753cf64a939188
yashwanthN24 <106897859+yashwanthN24@users.noreply.github.com>
2024-08-15 12:29:55 +0530

    Time: 5 ms (52.83%) | Memory: 44.5 MB (44.72%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        else {
+            char arr1[] = s.toCharArray();
+            char arr2[] = t.toCharArray();
+
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            if(Arrays.equals(arr1 , arr2)){
+                return true ;
+            }else{
+                return false ; 
+            }
+        }
+    }
+}
