34c3e7c7d0571d0895b9c6bb625300f07cedbd5c
yashwanthN24 <106897859+yashwanthN24@users.noreply.github.com>
2024-09-13 10:13:41 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (83.17%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -15,27 +15,54 @@
  */
 class Solution {
 
-    public void inorderseq(TreeNode root , ArrayList<Integer> seq){
-        
-        if(root == null)
-            return ; 
+    // Approach 1 
 
-        inorderseq(root.left, seq);
-        seq.add(root.val);
-        inorderseq(root.right , seq);
-    }
+    // public void inorderseq(TreeNode root , ArrayList<Integer> seq){
+        
+    //     if(root == null)
+    //         return ; 
+
+    //     inorderseq(root.left, seq);
+    //     seq.add(root.val);
+    //     inorderseq(root.right , seq);
+    // }
+
+    // public boolean isValidBST(TreeNode root) {
+    //     ArrayList<Integer> inorder = new ArrayList<>() ; 
+    //     inorderseq(root  , inorder);
+
+    //     for(int i = 0 ; i<inorder.size()-1; i++){
+    //         if(inorder.get(i) >= inorder.get(i+1)){
+    //             return false ; 
+    //         }
+    //     }
+
+
+    //     return true ; 
+    // }
+
+
+    // Approach  2
+
 
     public boolean isValidBST(TreeNode root) {
-        ArrayList<Integer> inorder = new ArrayList<>() ; 
-        inorderseq(root  , inorder);
+        return ValidateBst(root , null , null);
+    }
 
-        for(int i = 0 ; i<inorder.size()-1; i++){
-            if(inorder.get(i) >= inorder.get(i+1)){
-                return false ; 
-            }
+    public boolean ValidateBst(TreeNode root , TreeNode min , TreeNode max){
+        if(root == null){
+            return true ; 
         }
 
+        if(min != null && root.val <= min.val){
+            return false ; 
+        }
 
-        return true ; 
+        if(max != null && root.val >= max.val){
+            return false ; 
+        }
+
+        return ValidateBst(root.left , min , root) && ValidateBst(root.right , root , max);
+    
     }
 }
