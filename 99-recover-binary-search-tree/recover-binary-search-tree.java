11d407721eb8f597e82d24cebe00027e6a713e4f
yashwanthN24 <106897859+yashwanthN24@users.noreply.github.com>
2024-09-13 16:25:32 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (32.08%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -18,44 +18,90 @@ class Solution {
     // Approach 1 : Brute Force Time Complexity : O(2n) + O(nlogn) => O(nlogn)
     // Space complexity : O(n) due to extra arrayList creation , Apart from auxillary  recursion Stack space
 
+    // public void recoverTree(TreeNode root) {
+    //     ArrayList<Integer> inorderseq = new ArrayList<>();
+    //     getInorder(root , inorderseq);
+
+    //     // correct inorder seqence 
+    //     Collections.sort(inorderseq);
+
+    //     traverseInorder(root , inorderseq);
+
+    // }
+
+    // public void getInorder(TreeNode root , ArrayList<Integer> list){
+        
+    //     if(root == null){
+    //         return ;
+    //     }
+
+    //     getInorder(root.left , list);
+    //     list.add(root.val);
+    //     getInorder(root.right , list);
+
+    // }
+
+    // int i = 0;
+    // public void traverseInorder(TreeNode root , ArrayList<Integer> correctseq){
+    //     if(root == null){
+    //         return ; 
+    //     }
+
+    //     traverseInorder(root.left , correctseq);
+    //     if(root.val != correctseq.get(i)){
+    //         int temp = root.val; 
+    //         root.val = correctseq.get(i);
+    //         correctseq.set(i  , temp );
+            
+    //     }
+    //     i++;
+    //     traverseInorder(root.right , correctseq);
+    // }
+
+
+
+    // Approach 2 - O(n) only inorder traversal
+    TreeNode first ,prev ,  middle , last ; 
     public void recoverTree(TreeNode root) {
-        ArrayList<Integer> inorderseq = new ArrayList<>();
-        getInorder(root , inorderseq);
+        first = middle = last = null;
+        prev = new TreeNode(Integer.MIN_VALUE);
+        inorder(root);
 
-        // correct inorder seqence 
-        Collections.sort(inorderseq);
-
-        traverseInorder(root , inorderseq);
+        if(first!=null && last != null){
+            int temp = first.val ; 
+            first.val = last.val ; 
+            last.val = temp ; 
+        }else if(first != null && middle != null){
+            int temp = middle.val ; 
+            middle.val = first.val ; 
+            first.val = temp ; 
+        }
 
     }
 
-    public void getInorder(TreeNode root , ArrayList<Integer> list){
-        
+    public void inorder(TreeNode root){
         if(root == null){
             return ;
         }
 
-        getInorder(root.left , list);
-        list.add(root.val);
-        getInorder(root.right , list);
+        inorder(root.left);
 
-    }
+        if(prev != null && (root.val < prev.val )){
 
-    int i = 0;
-    public void traverseInorder(TreeNode root , ArrayList<Integer> correctseq){
-        if(root == null){
-            return ; 
+            if(first == null){
+                first = prev ; 
+                middle = root; 
+            }else{
+                last = root ; 
+            }
+
         }
 
-        traverseInorder(root.left , correctseq);
-        if(root.val != correctseq.get(i)){
-            int temp = root.val; 
-            root.val = correctseq.get(i);
-            correctseq.set(i  , temp );
-            
-        }
-        i++;
-        traverseInorder(root.right , correctseq);
+        prev = root ; 
+        inorder(root.right);
     }
+
+
+
 
 }
