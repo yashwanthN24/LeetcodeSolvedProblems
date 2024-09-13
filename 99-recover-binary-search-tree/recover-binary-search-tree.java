8a2e48ff6d397e9e0ffac534f1b18fbd32a743db
yashwanthN24 <106897859+yashwanthN24@users.noreply.github.com>
2024-09-13 12:12:08 +0530

    Time: 3 ms (13.18%) | Memory: 44.1 MB (87.70%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void recoverTree(TreeNode root) {
+        ArrayList<Integer> inorderseq = new ArrayList<>();
+        getInorder(root , inorderseq);
+
+        // correct inorder seqence 
+        Collections.sort(inorderseq);
+
+        traverseInorder(root , inorderseq);
+
+    }
+
+    public void getInorder(TreeNode root , ArrayList<Integer> list){
+        
+        if(root == null){
+            return ;
+        }
+
+        getInorder(root.left , list);
+        list.add(root.val);
+        getInorder(root.right , list);
+
+    }
+
+    int i = 0;
+    public void traverseInorder(TreeNode root , ArrayList<Integer> correctseq){
+        if(root == null){
+            return ; 
+        }
+
+        traverseInorder(root.left , correctseq);
+        if(root.val != correctseq.get(i)){
+            int temp = root.val; 
+            root.val = correctseq.get(i);
+            correctseq.set(i  , temp );
+            
+        }
+        i++;
+        traverseInorder(root.right , correctseq);
+    }
+
+}
