b2e4259b4f6a8723df44b2f4970cfe03eda36ad1
yashwanthN24 <106897859+yashwanthN24@users.noreply.github.com>
2024-09-14 23:47:52 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (66.72%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -1,8 +1,23 @@
 class Solution {
     public int getSum(int a, int b) {
-        if(b == 0){
-            return a ; 
-        }
-        return getSum(a ^ b , (a & b) << 1) ; 
+        // if(b == 0){
+        //     return a ; 
+        // }
+        // return getSum(a ^ b , (a & b) << 1) ; 
+
+
+
+        // 
+
+        if (a == 0) return b;
+	// if (b == 0) return a;
+
+	while (b != 0) {
+		int carry = a & b;
+		a = a ^ b;
+		b = carry << 1;
+	}
+	
+	return a;
     }
 }
