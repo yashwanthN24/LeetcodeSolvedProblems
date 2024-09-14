452d23d1507bd09e95d198c8e38312de6c39eb79
yashwanthN24 <106897859+yashwanthN24@users.noreply.github.com>
2024-09-14 19:34:07 +0530

    Time: 4 ms (98.56%) | Memory: 52 MB (23.86%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -7,3 +7,18 @@ class Solution {
          return list;
     }
 }
+
+
+//  n = 2 
+
+
+// 0 to 2^n-1 
+
+// 0 1 2 3 i/p 
+// 0 1 3 2 o/p do XOR
+// ------------
+
+// 0 0 1 1  // this should XOR with i/p to get o/p this sequence is nothing but i>> 2 where i = 0 to 2^n-1 ;
+
+
+// so its i ^ (i >> 1); this is  the number in output add to list
