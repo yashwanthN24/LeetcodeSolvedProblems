aaf5d8b8adf4cd546b5288dfd0f9f8273f34d686
yashwanthN24 <106897859+yashwanthN24@users.noreply.github.com>
2024-09-14 19:18:59 +0530

    Time: 1 ms (99.81%) | Memory: 42.4 MB (28.34%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,10 +1,28 @@
 import java.math.BigInteger;
 class Solution {
     public String addBinary(String a, String b) {
-        return (new BigInteger(a,2).add(new BigInteger(b,2))).toString(2);
+        // return (new BigInteger(a,2).add(new BigInteger(b,2))).toString(2);
+
+
+        // or  APproach 2
+
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+        if(i >= 0) carry += a.charAt(i--) - '0';
+        if(j >= 0) carry += b.charAt(j--) - '0';
+        sb.append(carry % 2);
+        carry /= 2;
+        }
+        return sb.reverse().toString();
     }
 }
 
 // BigInteger can handle numbers in base 2 , 8 and 10 
 
 // new BigInteger(a , 2 ) creates decimal of a 
+
+// toString(2) converts the decimal sum into binary string i.e base 2 
